11221f7ff02a1f8c22aa63c8b662a7f478b9e5e2
Maciej Lewandowski <maciej@encrypted.pl>
2014-09-03 22:09:48 +0200

    Language change. Redirect after warehouse/add/sent change

--- application/controllers/warehouse/add.php
+++ application/controllers/warehouse/add.php
@@ -62,7 +62,7 @@ class Add extends CI_Controller {
 		
 		if($this->input->post('sent')) {
 				$this->log_model->addAction($this->input->post('id'), (0 - $this->input->post('amount')), 4);
-				redirect('main/status','refresh');
+				redirect('warehouse/add/sent','refresh');
 			} else {
 				$this->load->view('warehouse/add/sent_view', $data);
 			}
