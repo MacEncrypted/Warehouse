020a0b5a28a018e852a10e8e54c0d3e70cf120d9
Maciej Lewandowski <maciej@encrypted.pl>
2014-11-20 18:55:15 +0100

    Updated database scripts to 2.0

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,8 +1,10 @@
---
--- Structure for table  `log`
---
+-- ----------
+-- Warehouse by Encrypted.pl
+-- Table: log
+-- ----------
+DROP TABLE log;
 
-CREATE TABLE IF NOT EXISTS `log` (
+CREATE TABLE `log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_user` int(11) NOT NULL,
@@ -12,15 +14,15 @@ CREATE TABLE IF NOT EXISTS `log` (
   `id_packing` int(11) NOT NULL,
   `id_order` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=357 DEFAULT CHARSET=latin2;
 
--- --------------------------------------------------------
+-- ----------
+-- Warehouse by Encrypted.pl
+-- Table: log_history
+-- ----------
+DROP TABLE log_history;
 
---
--- Structure for table  `log_history`
---
-
-CREATE TABLE IF NOT EXISTS `log_history` (
+CREATE TABLE `log_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_user` int(11) NOT NULL,
@@ -30,15 +32,15 @@ CREATE TABLE IF NOT EXISTS `log_history` (
   `id_packing` int(11) NOT NULL,
   `info` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=338 DEFAULT CHARSET=latin2;
 
--- --------------------------------------------------------
+-- ----------
+-- Warehouse by Encrypted.pl
+-- Table: notes
+-- ----------
+DROP TABLE notes;
 
---
--- Structure for table  `notes`
---
-
-CREATE TABLE IF NOT EXISTS `notes` (
+CREATE TABLE `notes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `data` date NOT NULL,
   `title` text NOT NULL,
@@ -46,69 +48,64 @@ CREATE TABLE IF NOT EXISTS `notes` (
   `id_user` int(11) NOT NULL,
   `deleted` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin2;
 
--- --------------------------------------------------------
+-- ----------
+-- Warehouse by Encrypted.pl
+-- Table: orders
+-- ----------
+DROP TABLE orders;
 
---
--- Structure for table  `packings`
---
-
-CREATE TABLE IF NOT EXISTS `packings` (
+CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text NOT NULL,
   `deleted` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------
+-- Warehouse by Encrypted.pl
+-- Table: packings
+-- ----------
+DROP TABLE packings;
 
---
--- Structure for table  `orders`
---
-
-CREATE TABLE IF NOT EXISTS `orders` (
+CREATE TABLE `packings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text NOT NULL,
   `deleted` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=44 DEFAULT CHARSET=latin2;
 
--- --------------------------------------------------------
+-- ----------
+-- Warehouse by Encrypted.pl
+-- Table: products
+-- ----------
+DROP TABLE products;
 
---
--- Structure for table  `products`
---
-
-CREATE TABLE IF NOT EXISTS `products` (
+CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text NOT NULL,
   `deleted` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=44 DEFAULT CHARSET=latin2;
 
--- --------------------------------------------------------
+-- ----------
+-- Warehouse by Encrypted.pl
+-- Table: users
+-- ----------
+DROP TABLE users;
 
---
--- Structure for table  `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(20) NOT NULL,
   `passwd` varchar(32) NOT NULL,
   `level` int(11) NOT NULL,
   `deleted` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Table with users data.' AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='Table with users data.';
 
--- --------------------------------------------------------
-
---
--- Insert of primary Warehouse user.
---
-INSERT INTO `users` (`id`, `login`, `passwd`) VALUES ('1', 'root', '63a9f0ea7bb98050796b649e85481845');
+INSERT INTO users VALUES("1","root","63a9f0ea7bb98050796b649e85481845","2","0");
 
